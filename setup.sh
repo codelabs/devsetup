3ef55bd0ef48a38c07548f4f284ef27a2db9dcbc
Dileep Eduri <deduri@m0072415.lab.ppops.net>
2017-03-07 23:21:34 +0000

    fixing paths

--- setup.sh
+++ setup.sh
@@ -2,11 +2,11 @@
 
 set -x
 
-source yum_updates.sh
-source pip_install.sh
-source powerline.sh
-source vimsetup.sh
-source bashpro.sh
-source tmux.sh
-source perlbrew.sh
+source ./yum_updates.sh
+source ./pip_install.sh
+source ./powerline.sh
+source ./vimsetup.sh
+source ./bashpro.sh
+source ./tmux.sh
+source ./perlbrew.sh
 
