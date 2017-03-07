3a07bff09035b6d7c71289a5bdd426efeeda8d45
Dileep Eduri <deduri@m0072415.lab.ppops.net>
2017-03-07 23:28:49 +0000

    fixing pip

--- pip_install.sh
+++ pip_install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 wget --no-check-certificate https://bootstrap.pypa.io/get-pip.py
-chmod +x $HOME/get-pip.py
+chmod +x ./get-pip.py
 
 # install pip to user local dir -> $HOME/.local
-python get-pip.py --user
+python ./get-pip.py --user
 
